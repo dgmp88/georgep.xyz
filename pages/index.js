8f9fe48fc6d05ab8cb0b8be3b7ea4f807e234152
George Prichard <dgmprichard@gmail.com>
2022-04-03 16:14:10 +0100

    Styling fixes for mobile

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,20 @@ function Home() {
     <>
       <Background></Background>
       <Header />
-      <div className="card m-auto w-96 glass my-20">
-        <figure>
-          <img src="/images/me.jpg" alt="George"></img>
-        </figure>
-        <div className="card-body text-gray-100">
-          <h2 className="card-title">Hi. I'm George</h2>
-          <a className="link" href="https://linkedin.com/in/george-prichard">
-            LinkedIn
-          </a>
-          <a className="link" href="https://github.com/dgmp88">
-            Github
-          </a>
+      <div className="max-w-md m-auto my-10">
+        <div className="card m-3 glass p-3">
+          <figure>
+            <img className="rounded-lg" src="/images/me.jpg" alt="George"></img>
+          </figure>
+          <div className="card-body text-gray-100">
+            <h2 className="card-title">Hi. I'm George</h2>
+            <a className="link" href="https://linkedin.com/in/george-prichard">
+              LinkedIn
+            </a>
+            <a className="link" href="https://github.com/dgmp88">
+              Github
+            </a>
+          </div>
         </div>
       </div>
     </>
