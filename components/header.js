b66462113013983a612a6b01c4e1909ef776b4e7
George Prichard <dgmprichard@gmail.com>
2022-04-03 16:34:23 +0100

    Better styling fix- disable dark mode

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export function Header() {
   return (
     <div className="navbar">
-      <div className="flex-1 text-black">
+      <div className="flex-1">
         <Link href="/">
           <a className="btn btn-ghost normal-case text-xl">George Prichard</a>
         </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,7 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    darkTheme: 'light',
+  },
 };
