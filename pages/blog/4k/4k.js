c6d6da8d40a7ce19be163c6df8f6484cb4465913
George Prichard <dgmprichard@gmail.com>
2022-04-04 16:52:02 +0100

    Add 4k weeks blog post

---
+++ pages/blog/4k/4k.js
@@ -0,0 +1,121 @@
+import { colors, lerpColors, lerpPalette } from './colors.js';
+import { Component } from 'react';
+
+import _ from 'lodash';
+class FourKWeeks extends Component {
+  componentDidMount() {
+    var canvas = document.getElementById('canvas');
+    const w = 600;
+    const h = 800;
+
+    canvas.width = w;
+    canvas.height = h;
+
+    const palette = colors.coolorsROYGBIV;
+
+    const ctx = canvas.getContext('2d');
+
+    const lifeExpect = 80;
+
+    // Fill the background
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    function drawDots(frac) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      let yN = lifeExpect;
+      let xN = 52;
+      let r = 4;
+      let m = 3 * r;
+      let xS = (w - m * 2) / (xN - 1);
+      let yS = (h - m * 2) / (yN - 1);
+      let idx = 1;
+      const tot = yN * xN;
+      let maybeFirstCircle = true;
+      if (frac == 0) {
+        maybeFirstCircle = false;
+      }
+      for (let y = 0; y < yN; y += 1) {
+        let y1 = y * yS + m;
+        for (let x = 0; x < xN; x += 1) {
+          let color = lerpPalette(palette, y / yN / 2 + x / xN / 2);
+
+          let x1 = x * xS + m;
+          let r_ = r;
+
+          let draw;
+          if (idx / tot < frac) {
+            draw = () => ctx.stroke();
+          } else {
+            if (maybeFirstCircle) {
+              r_ *= 0.5;
+              maybeFirstCircle = false;
+            }
+            draw = () => ctx.fill();
+          }
+
+          // Draw a circle
+          ctx.beginPath();
+          ctx.ellipse(x1, y1, r_, r_, 0, 0, Math.PI * 2);
+
+          ctx.fillStyle = color;
+          ctx.strokeStyle = color;
+          draw();
+          idx += 1;
+        }
+      }
+    }
+
+    drawDots(0);
+
+    let drawTimeout = 1000;
+    let drawTimer = null;
+
+    var dobInput = document.getElementById('dob');
+    function dateChange() {
+      let dob = new Date(dobInput.value);
+      let ageMs = new Date() - dob;
+      var millisecondsPerDay = 24 * 60 * 60 * 1000;
+
+      let ageYears = ageMs / millisecondsPerDay / 365.25;
+      let timeFrac = ageYears / lifeExpect;
+      if ((timeFrac > 0) & (timeFrac < 1)) {
+        clearTimeout(drawTimer);
+        drawTimer = setTimeout(() => {
+          drawDots(timeFrac);
+        }, drawTimeout);
+      }
+    }
+
+    dobInput.addEventListener('change', dateChange);
+
+    // setTimeout(() => {
+    //   dobInput.value = '2000-01-01';
+    //   dateChange();
+    // });
+  }
+  render() {
+    return (
+      <div id="app" className="m-5">
+        <div className="flex">
+          <div className="w-1/4"></div>
+          <div className="w-1/4">Your DOB</div>
+          <div className="w-1/4">
+            <input
+              type="date"
+              id="dob"
+              name="trip-start"
+              min="1900-01-01"
+              max="2020-12-31"
+            />
+          </div>
+        </div>
+        <div>
+          <canvas id="canvas" className="m-auto"></canvas>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { FourKWeeks };
---
+++ pages/blog/4k/colors.js
@@ -0,0 +1,128 @@
+const colors = {
+  // https://www.colourlovers.com/palette/848743/(_%E2%80%9D_)
+
+  sugar: "#490A3D,#BD1550,#E97F02,#F8CA00,#8A9B0F,#490A3D,#BD1550,#E97F02",
+
+  //https://www.colourlovers.com/palette/1534867/Magic_Forest
+  magic_forest:
+    "#014152,#0C7871,#219C85,#48A882,#76B07A,#014152,#0C7871,#219C85",
+
+  //https://www.colourlovers.com/palette/1239681/Galaxy_glow
+  galaxy: "#4CA5A9,#46727F,#414A6D,#383158,#1C1925,#4CA5A9,#46727F,#414A6D",
+
+  // https://www.colourlovers.com/palette/2937476/Galaxies_in_a_Jar
+  galaxy2: "#20345B,#C4365B,#E66E6C,#E9B08C,#F1E4A8,#20345B,#C4365B,#E66E6C",
+
+  // https://coolors.co/palette/001219-005f73-0a9396-94d2bd-e9d8a6-ee9b00-ca6702-bb3e03-ae2012-9b2226
+  coolors1: [
+    "001219",
+    "005f73",
+    "0a9396",
+    "94d2bd",
+    "e9d8a6",
+    "ee9b00",
+    "ca6702",
+    "bb3e03",
+    "ae2012",
+    "9b2226",
+  ],
+
+  // https://coolors.co/palette/d9ed92-b5e48c-99d98c-76c893-52b69a-34a0a4-168aad-1a759f-1e6091-184e77
+  coolors2: [
+    "99d98c",
+    "76c893",
+    "52b69a",
+    "34a0a4",
+    "168aad",
+    "1a759f",
+    "1e6091",
+    "184e77",
+  ],
+
+  // https://coolors.co/f94144-f3722c-f8961e-f9c74f-90be6d-43aa8b-577590
+  coolorsROYGBIV: [
+    "f94144",
+    "f3722c",
+    "f8961e",
+    "f9c74f",
+    "90be6d",
+    "43aa8b",
+    "577590",
+  ],
+};
+function hexToRGBA(hex, alpha) {
+  if (typeof hex !== "string") {
+    return;
+  }
+  var r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) {
+    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+  } else {
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
+}
+
+function hexToParts(hex) {
+  var col = {
+    r: parseInt(hex.slice(1, 3), 16),
+    g: parseInt(hex.slice(3, 5), 16),
+    b: parseInt(hex.slice(5, 7), 16),
+  };
+
+  return col;
+}
+
+function lerpColors(c1, c2, amount) {
+  c1 = hexToParts(c1);
+  c2 = hexToParts(c2);
+
+  let r = c1.r - (c1.r - c2.r) * amount;
+  let g = c1.g - (c1.g - c2.g) * amount;
+  let b = c1.b - (c1.b - c2.b) * amount;
+
+  r = Math.round(r);
+  g = Math.round(g);
+  b = Math.round(b);
+
+  return `rgb(${r},${g},${b})`;
+}
+
+function lerpNSteps(c1, c2, n) {
+  let cols = [];
+  for (let i = 0; i < n; i++) {
+    let frac = i / (n - 1);
+    cols.push(lerpColors(c1, c2, frac));
+  }
+  return cols;
+}
+
+function lerpPalette(palette, amount) {
+  const n = palette.length;
+  if (amount === 1) {
+    // As we don't have a next color, lazy hack
+    amount -= 0.00001;
+  }
+  const c1Idx = Math.floor(amount * (n - 1));
+  const c2Idx = c1Idx + 1;
+  const c1 = palette[c1Idx];
+  const c2 = palette[c2Idx];
+  const gaps = 1 / (n - 1);
+  const cAmount = (amount % gaps) / gaps;
+
+  return lerpColors(c1, c2, cAmount);
+}
+
+for (const [key, value] of Object.entries(colors)) {
+  if (typeof value === "string") {
+    // colorlovers
+    colors[key] = value.split(",");
+  } else if (typeof value === "object") {
+    // coolors
+    colors[key] = value.map((x) => "#" + x);
+  }
+}
+
+export { colors, hexToRGBA, lerpColors, lerpNSteps, lerpPalette };
