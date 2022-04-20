6e2132baf5a02c46642a11a284093ed763ea4db1
George Prichard <dgmprichard@gmail.com>
2022-04-20 10:19:48 +0100

    Circles!

--- components/pixi.js
+++ components/pixi.js
@@ -2,42 +2,12 @@ import { useEffect, Component, createRef } from 'react';
 import * as PIXI from 'pixi.js';
 import chroma from 'chroma-js';
 
-function initPixi() {
-  console.log(this);
-  let app = new PIXI.Application({
-    resizeTo: window,
-    antialias: true,
-
-    backgroundColor: 0xffffff,
-  });
-  // document.body.appendChild(app.view);
-
-  let cscale = chroma
-    .scale(['#2175D8', '#DC5DCE', '#CC223D', '#F07414', '#FDEE61', '#74C425'])
-    .mode('lch');
-
-  const x = window.innerWidth,
-    y = window.innerHeight;
-
-  let r = x / 2;
-  let n = 11;
-
-  for (let i = 0; i < n; i += 1) {
-    const frac = 1 - i / n;
-    const size = r * frac;
-    const gr = new PIXI.Graphics();
-    gr.beginFill(PIXI.utils.string2hex(cscale(frac).hex()));
-    gr.drawCircle(x / 2, y / 2, r * frac);
-    gr.endFill();
-    app.stage.addChild(gr);
-  }
-
-  // // Add a ticker callback to move the sprite back and forth
-  // let elapsed = 0.0;
-  // app.ticker.add((delta) => {
-  //   elapsed += delta;
-  //   sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-  // });
+const cols = ['#2175D8', '#DC5DCE', '#CC223D', '#F07414', '#FDEE61', '#74C425'];
+function getCircleCoords(x, y, r, deg) {
+  let t = (deg / 360) * 2 * Math.PI;
+  x = r * Math.cos(t) + x;
+  y = r * Math.sin(t) + y;
+  return { x, y };
 }
 
 class Pixi extends Component {
@@ -45,17 +15,79 @@ class Pixi extends Component {
     super(props);
     this.canvasRef = createRef();
   }
+
+  initPixi() {
+    let app = new PIXI.Application({
+      resizeTo: window,
+      antialias: true,
+      view: this.canvasRef.current,
+      backgroundColor: PIXI.utils.string2hex(cols[cols.length - 1]),
+    });
+    const { view, renderer } = app;
+
+    let cscale = chroma.scale(cols).mode('lch');
+
+    const w = window.innerWidth,
+      h = window.innerHeight;
+
+    let r = w * 0.5;
+    let n = 50;
+
+    let circles = {};
+    let x = w / 2;
+    let y = h / 2;
+    let pow = 1.8;
+
+    for (let idx = 0; idx < n; idx += 1) {
+      const frac = 1 - idx / n;
+      const size = r * frac;
+      const gr = new PIXI.Graphics();
+      gr.beginFill(PIXI.utils.string2hex(cscale(frac).hex()));
+      gr.drawCircle(0, 0, size);
+      gr.endFill();
+
+      let texture = renderer.generateTexture(gr);
+      let sprite = new PIXI.Sprite(texture);
+      sprite.anchor.set(0.5);
+      let offset = idx ** pow;
+      let xy = getCircleCoords(x, y, 200, (idx / n) * 360 + offset);
+      sprite.x = xy.x;
+      sprite.y = xy.y;
+      app.stage.addChild(sprite);
+
+      circles[idx] = sprite;
+    }
+
+    // Add a ticker callback to move the sprite back and forth
+    let elapsed = 0.0;
+    app.ticker.add((delta) => {
+      elapsed += delta;
+      for (const [idx, circle] of Object.entries(circles)) {
+        let offset = idx ** pow;
+        let xy = getCircleCoords(
+          x,
+          y,
+          200,
+          (idx / n) * 360 + elapsed * 0.1 + offset
+        );
+        circle.x = xy.x;
+        circle.y = xy.y;
+        circle.x;
+      }
+    });
+  }
+
   componentDidMount() {
     if (typeof window === 'object') {
       let type = 'WebGL';
       if (!PIXI.utils.isWebGLSupported()) {
         type = 'canvas';
       }
-      initPixi(this);
+      this.initPixi();
     }
   }
   render() {
-    return <canvas id={this.canvasRef}></canvas>;
+    return <canvas ref={this.canvasRef}></canvas>;
   }
 }
 
