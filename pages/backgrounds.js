f059a59c7811c71eb8f1e093c0d4b75b42b3a20d
George Prichard <dgmprichard@gmail.com>
2022-04-21 10:55:07 +0100

    Better refreshing, downloading, control # of triangles

--- pages/backgrounds.js
+++ pages/backgrounds.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import { SVG } from '@svgdotjs/svg.js';
 import Delaunator from 'delaunator';
 import chroma from 'chroma-js';
-
+const defaultN = 500;
 const lightRainbow = [
   '#2175D8',
   '#DC5DCE',
@@ -28,14 +28,31 @@ const darkRainbow = [
 const retiPage = ['#03045E', '#11488e', '#032550'];
 
 class Triangles {
-  constructor(n, w, h, draw, border = 200, cols = darkRainbow) {
+  refreshPause = 200;
+  refreshTimeout;
+  constructor(n = defaultN, border = 200, colors = lightRainbow) {
     this.n = n;
-    this.w = w;
-    this.h = h;
-    this.draw = draw;
     this.border = border;
-    this.cscale = chroma.scale(cols).mode('lch');
+    this.colors = colors;
+    this.cscale = chroma.scale(colors).mode('lch');
+
+    this.draw = SVG().addTo('#svg');
+    this.setSize();
     this.setPoints();
+    window.onresize = () => {
+      window.setTimeout(() => {
+        this.setSize();
+        this.drawTriangles();
+      }),
+        500;
+    };
+  }
+
+  setSize() {
+    this.w = window.innerWidth;
+    this.h = window.innerHeight;
+
+    this.draw.size(this.w, this.h);
   }
 
   edgesOfTriangle(t) {
@@ -56,7 +73,6 @@ class Triangles {
       points.push([getRnd(this.w), getRnd(this.h)]);
     }
 
-    this.orig_points = points;
     this.points = points;
   }
 
@@ -76,49 +92,127 @@ class Triangles {
         .stroke({ width: 1, color: col.hex() });
     }
   }
-}
 
-function download(data, filename, type) {
-  var file = new Blob([data], { type: type });
-  // Others
-  var a = document.createElement('a'),
-    url = URL.createObjectURL(file);
-  a.href = url;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(function () {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 0);
+  refreshAfterPause() {
+    // Ref
+    clearTimeout(this.refreshTimeout);
+    this.refreshTimeout = setTimeout(() => {
+      this.refresh();
+    }, this.refreshPause);
+  }
+
+  refresh() {
+    this.draw.clear();
+    this.setPoints();
+    this.drawTriangles();
+  }
+
+  setN(n) {
+    this.n = n;
+  }
+
+  download() {
+    let data = this.draw.svg();
+    let filename = 'background.svg';
+    let type = 'plain/text';
+    var file = new Blob([data], { type: type });
+    // Others
+    var a = document.createElement('a'),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
 }
 
 class Background extends Component {
-  mounted = false;
+  constructor(props) {
+    super(props);
+    this.state = { mounted: false };
+    this.download = this.download.bind(this);
+    this.refresh = this.refresh.bind(this);
+    this.changeN = this.changeN.bind(this);
+  }
   componentDidMount() {
-    if (this.mounted) {
+    if (this.state.mounted) {
       return;
     }
-    const w = window.innerWidth,
-      h = window.innerHeight;
-    var draw = SVG().addTo('#svg').size(w, h);
 
-    var triangles = new Triangles(400, w, h, draw);
-
+    let triangles = new Triangles();
     triangles.drawTriangles();
 
-    this.mounted = true;
+    this.state.triangles = triangles;
+    this.state.mounted = true;
+  }
 
-    let dld = () => download(draw.svg(), 'background.svg', 'plain/text');
+  download() {
+    this.state.triangles.download();
+  }
 
-    window.onmousedown = dld;
-    // window.onmousemove = (x, y) => triangles.mouseMoved(event);
+  refresh() {
+    this.state.triangles.refresh();
+  }
+
+  changeN(event) {
+    let n = parseInt(event.target.value);
+    this.state.triangles.setN(n);
+    this.state.triangles.refreshAfterPause();
   }
 
   render() {
     return (
       <>
-        <div id="svg"></div>
+        <div className="absolute -z-1" id="svg"></div>
+        <div className="hero min-h-screen">
+          <div className="hero-content text-center">
+            <div
+              className="rounded p-5"
+              style={{ 'background-color': 'rgba(255, 255, 255, 0.5)' }}
+            >
+              <h1 className="text-3xl font-bold">Background Designer</h1>
+              <p className="py-6">
+                Resize the window to get the export size you want
+              </p>
+              <div>
+                <div className="font-medium">Number of triangles</div>
+
+                <input
+                  type="range"
+                  min="20"
+                  max="3000"
+                  defaultValue={defaultN}
+                  className="range"
+                  onChange={this.changeN}
+                ></input>
+              </div>
+
+              <div className="pb-2">
+                <div className="font-medium">Colors</div>
+                {/* <div>
+                  {this.state.triangles.colors.map((item) => (
+                    <input
+                      type="text"
+                      className="input-sm w-24 mx-2"
+                      value="a"
+                    ></input>
+                  ))}
+                </div> */}
+              </div>
+
+              <button className="btn btn-secondary" onClick={this.refresh}>
+                Refresh
+              </button>
+              <button className="btn btn-primary" onClick={this.download}>
+                Download SVG
+              </button>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
