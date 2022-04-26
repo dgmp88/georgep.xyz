f97ff8267715261d4e6b10aa6f81030defe424a8
George Prichard <dgmprichard@gmail.com>
2022-04-26 23:46:16 +0100

    Sharing logic between lines and triangles

---
+++ components/backgrounds/parts.jsx
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+import chroma from 'chroma-js';
+import * as colorUtils from '../../lib/colors';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faXmark,
+  faPlus,
+  faFileImport,
+} from '@fortawesome/free-solid-svg-icons';
+
+function PasteModal({ setColors }) {
+  return (
+    <>
+      <input type="checkbox" id="my-modal" className="modal-toggle"></input>
+      <div className="modal">
+        <div className="modal-box">
+          <p className="py-4">
+            Paste colors below. Try{' '}
+            <a className="link" href="https://coolors.co/" target="_blank">
+              coolors
+            </a>{' '}
+            or{' '}
+            <a
+              className="link"
+              href="https://www.colourlovers.com/"
+              target="_blank"
+            >
+              Colour Lovers
+            </a>{' '}
+            .{' '}
+          </p>
+          <textarea
+            id="colorTextArea"
+            className="bg-slate-200 w-full"
+            placeholder="#B6211B,#E77833,#ECD817,#98E1F2"
+          ></textarea>
+          <p>Or try these:</p>
+          {colorUtils.examplePalettes.map((palette, idx) => (
+            <Palette
+              key={idx}
+              colors={palette}
+              clickHandler={() => {
+                let cols = colorUtils.getColsFromString(palette);
+                setColors(cols);
+              }}
+            ></Palette>
+          ))}
+          <div className="modal-action">
+            <label
+              htmlFor="my-modal"
+              className="btn"
+              onClick={() => {
+                const text = document.getElementById('colorTextArea').value;
+
+                let newCols = colorUtils.getColsFromString(text);
+                if (newCols.length > 1) {
+                  setColors(newCols);
+                }
+              }}
+            >
+              Done
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Color(props) {
+  const [color, setColor] = useState(props.color);
+  return (
+    <>
+      <input
+        type="color"
+        className="input-sm w-10 p-0 m-2 border-none"
+        value={color}
+        onChange={(event) => {
+          let c = event.target.value;
+          setColor(c);
+        }}
+        onBlur={(event) => {
+          props.onColorUpdate(color);
+        }}
+      ></input>
+    </>
+  );
+}
+
+function Colors({ colors, setColors }) {
+  return (
+    <>
+      <div className="font-medium">Colors</div>
+      <div className="flex flex-wrap justify-center align-middle">
+        {colors.map((color, idx) => (
+          <div key={color} className="px-1">
+            <Color
+              color={color}
+              onColorUpdate={(col) => {
+                colors[idx] = col;
+                setColors([...colors]);
+              }}
+            />
+            <span
+              className="inline-block align-top cursor-pointer 
+              "
+              onClick={() => {
+                let colorsTmp = [...colors];
+                colorsTmp.splice(idx, 1);
+                setColors(colorsTmp);
+              }}
+            >
+              <FontAwesomeIcon icon={faXmark} />
+            </span>
+          </div>
+        ))}
+      </div>
+      <span
+        className="btn btn-secondary btn-xs m-3"
+        onClick={(event) => {
+          let colorsTmp = [...colors];
+          colorsTmp.push(chroma.random().hex());
+          setColors(colorsTmp);
+        }}
+      >
+        <FontAwesomeIcon icon={faPlus} />
+      </span>
+      <label
+        htmlFor="my-modal"
+        className="btn btn-secondary btn-xs m-3 modal-button"
+      >
+        <FontAwesomeIcon icon={faFileImport} />
+      </label>
+      <PasteModal setColors={setColors}></PasteModal>
+    </>
+  );
+}
+
+function NumberSlider({ defaultNumber, setNumber, min, max }) {
+  return (
+    <>
+      <div>
+        <div className="font-medium">Number of triangles</div>
+        <input
+          type="range"
+          min={min}
+          max={max}
+          defaultValue={defaultNumber}
+          className="range"
+          onChange={(event) => {
+            let n = parseInt(event.target.value);
+            setNumber(n);
+          }}
+        ></input>
+      </div>
+    </>
+  );
+}
+
+function Palette(props) {
+  const colors = props.colors.split(',');
+  return (
+    <>
+      <div
+        className="flex flex-wrap justify-center bg-gray-100 m-2 rounded cursor-pointer"
+        onClick={() => props.clickHandler(colors)}
+      >
+        {colors.map((color, idx) => (
+          <div
+            key={idx}
+            className="m-2 w-4 h-4 rounded-xl"
+            style={{
+              backgroundColor: color,
+            }}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export { NumberSlider, Color, Colors, Palette };
--- components/backgrounds/triangles.jsx
+++ components/backgrounds/triangles.jsx
@@ -4,19 +4,47 @@ import { SVG } from '@svgdotjs/svg.js';
 import * as colorUtils from '../../lib/colors';
 import { Triangles } from '../../lib/triangles';
 
+import { Color, Colors, NumberSlider } from './parts';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faArrowsRotate,
-  faDownload,
-  faXmark,
-  faPlus,
-  faFileImport,
-} from '@fortawesome/free-solid-svg-icons';
+import { faArrowsRotate, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const defaultNTriangles = 500;
 const defaultColors = colorUtils.darkRainbow;
 let defaultEdgeCol = '#ee9b00'; // with some opacity
 let edgesOn = false;
+
+function Edges({ edges, setEdges, edgeCol, setEdgeCol }) {
+  return (
+    <>
+      <div className="flex">
+        <div className="form-control w-1/3">
+          <label className="label cursor-pointer">
+            <span className="label-text font-medium">Edges</span>
+            <input
+              type="checkbox"
+              className="toggle"
+              onChange={() => {
+                setEdges(!edges);
+              }}
+              value={edges}
+            ></input>
+          </label>
+        </div>
+        <div
+          className={`w-2/3 text-right ${edges ? 'opacity-1' : 'opacity-0'}`}
+        >
+          <Color
+            color={edgeCol}
+            onColorUpdate={(col) => {
+              setEdgeCol(col);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
 
 export function TrianglesApp() {
   const [nTriangles, setNTriangles] = useState(defaultNTriangles);
@@ -77,7 +105,15 @@ export function TrianglesApp() {
   return (
     <>
       <NumberSlider
-        setNTriangles={(n) => runAfterPause(() => setNTriangles(n))}
+        min={3}
+        max={10000 ** 0.5}
+        defaultNumber={defaultNTriangles ** 0.5}
+        setNumber={(n) =>
+          runAfterPause(() => {
+            console.log('running', n ** 2);
+            setNTriangles(n ** 2);
+          })
+        }
       ></NumberSlider>
 
       <div className="pb-2">
@@ -101,209 +137,6 @@ export function TrianglesApp() {
         <button className="btn btn-secondary m-2" onClick={download}>
           <FontAwesomeIcon icon={faDownload} />
         </button>
-      </div>
-    </>
-  );
-}
-
-function Palette(props) {
-  const colors = props.colors.split(',');
-  return (
-    <>
-      <div
-        className="flex flex-wrap justify-center bg-gray-100 m-2 rounded cursor-pointer"
-        onClick={() => props.clickHandler(colors)}
-      >
-        {colors.map((color, idx) => (
-          <div
-            key={idx}
-            className="m-2 w-4 h-4 rounded-xl"
-            style={{
-              backgroundColor: color,
-            }}
-          ></div>
-        ))}
-      </div>
-    </>
-  );
-}
-
-function Color(props) {
-  const [color, setColor] = useState(props.color);
-  return (
-    <>
-      <input
-        type="color"
-        className="input-sm w-10 p-0 m-2 border-none"
-        value={color}
-        onChange={(event) => {
-          let c = event.target.value;
-          setColor(c);
-        }}
-        onBlur={(event) => {
-          props.onColorUpdate(color);
-        }}
-      ></input>
-    </>
-  );
-}
-
-function PasteModal({ setColors }) {
-  return (
-    <>
-      <input type="checkbox" id="my-modal" className="modal-toggle"></input>
-      <div className="modal">
-        <div className="modal-box">
-          <p className="py-4">
-            Paste colors below. Try{' '}
-            <a className="link" href="https://coolors.co/" target="_blank">
-              coolors
-            </a>{' '}
-            or{' '}
-            <a
-              className="link"
-              href="https://www.colourlovers.com/"
-              target="_blank"
-            >
-              Colour Lovers
-            </a>{' '}
-            .{' '}
-          </p>
-          <textarea
-            id="colorTextArea"
-            className="bg-slate-200 w-full"
-            placeholder="#B6211B,#E77833,#ECD817,#98E1F2"
-          ></textarea>
-          <p>Or try these:</p>
-          {colorUtils.examplePalettes.map((palette, idx) => (
-            <Palette
-              key={idx}
-              colors={palette}
-              clickHandler={() => {
-                let cols = colorUtils.getColsFromString(palette);
-                setColors(cols);
-              }}
-            ></Palette>
-          ))}
-          <div className="modal-action">
-            <label
-              htmlFor="my-modal"
-              className="btn"
-              onClick={() => {
-                const text = document.getElementById('colorTextArea').value;
-
-                let newCols = colorUtils.getColsFromString(text);
-                if (newCols.length > 1) {
-                  setColors(newCols);
-                }
-              }}
-            >
-              Done
-            </label>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function Colors({ colors, setColors }) {
-  return (
-    <>
-      <div className="font-medium">Colors</div>
-      <div className="flex flex-wrap justify-center align-middle">
-        {colors.map((color, idx) => (
-          <div key={color} className="px-1">
-            <Color
-              color={color}
-              onColorUpdate={(col) => {
-                colors[idx] = col;
-                setColors([...colors]);
-              }}
-            />
-            <span
-              className="inline-block align-top cursor-pointer 
-              "
-              onClick={() => {
-                let colorsTmp = [...colors];
-                colorsTmp.splice(idx, 1);
-                setColors(colorsTmp);
-              }}
-            >
-              <FontAwesomeIcon icon={faXmark} />
-            </span>
-          </div>
-        ))}
-      </div>
-      <span
-        className="btn btn-secondary btn-xs m-3"
-        onClick={(event) => {
-          let colorsTmp = [...colors];
-          colorsTmp.push(chroma.random().hex());
-          setColors(colorsTmp);
-        }}
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </span>
-      <label
-        htmlFor="my-modal"
-        className="btn btn-secondary btn-xs m-3 modal-button"
-      >
-        <FontAwesomeIcon icon={faFileImport} />
-      </label>
-      <PasteModal setColors={setColors}></PasteModal>
-    </>
-  );
-}
-
-function NumberSlider({ setNTriangles }) {
-  return (
-    <>
-      <div>
-        <div className="font-medium">Number of triangles</div>
-        <input
-          type="range"
-          min="10"
-          max="100"
-          defaultValue={defaultNTriangles ** 0.5}
-          className="range"
-          onChange={(event) => {
-            let n = parseInt(event.target.value) ** 2;
-            setNTriangles(n);
-          }}
-        ></input>
-      </div>
-    </>
-  );
-}
-
-function Edges({ edges, setEdges, edgeCol, setEdgeCol }) {
-  return (
-    <>
-      <div className="flex">
-        <div className="form-control w-1/3">
-          <label className="label cursor-pointer">
-            <span className="label-text font-medium">Edges</span>
-            <input
-              type="checkbox"
-              className="toggle"
-              onChange={() => {
-                setEdges(!edges);
-              }}
-              value={edges}
-            ></input>
-          </label>
-        </div>
-        <div
-          className={`w-2/3 text-right ${edges ? 'opacity-1' : 'opacity-0'}`}
-        >
-          <Color
-            color={edgeCol}
-            onColorUpdate={(col) => {
-              setEdgeCol(col);
-            }}
-          />
-        </div>
       </div>
     </>
   );
