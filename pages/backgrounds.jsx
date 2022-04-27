c5bd47c4e94018bb70608d01d6be78066631ae84
George Prichard <dgmprichard@gmail.com>
2022-04-27 12:56:49 +0100

    WIP on click background

--- pages/backgrounds.jsx
+++ pages/backgrounds.jsx
@@ -10,6 +10,7 @@ const apps = {
 
 export default function Backgrounds() {
   const [current, setCurrent] = useState('lines');
+  const [uiHidden, setUiHidden] = useState(false);
 
   let app = apps[current];
   return (
@@ -17,19 +18,27 @@ export default function Backgrounds() {
       <div className="absolute -z-1" id="svg">
         {/* This is the actual background  */}
       </div>
-      <div className="hero min-h-screen">
-        <div className="hero-content text-center max-w-xl">
+      <div
+        className="hero min-h-screen"
+        onClick={() => {
+          console.log('hit');
+          // setUiHidden(~uiHidden);
+        }}
+      >
+        <div
+          className={`hero-content text-center max-w-xl ${
+            uiHidden ? 'invisible' : 'visible'
+          }`}
+        >
           <div className="rounded p-5 bg-base-100 bg-opacity-75">
             <h1 className="text-3xl font-bold">Background Designer</h1>
-            <p className="py-6">
-              Resize the window to get the export size you want
-            </p>
-            <div className="tabs justify-center">
+
+            <div className="tabs justify-center py-2">
               {Object.keys(apps).map((name) => {
                 return (
                   <div
                     key={name}
-                    className={`tab tab-lifted ${
+                    className={`tab tab-bordered ${
                       name === current ? 'tab-active' : ''
                     }`}
                     onClick={() => setCurrent(name)}
@@ -39,9 +48,10 @@ export default function Backgrounds() {
                 );
               })}
             </div>
-
-            {/* <TrianglesApp></TrianglesApp> */}
-            {/* <LinesApp></LinesApp> */}
+            <div className="py-6 prose">
+              <p>Resize the window to get the export size you want.</p>
+              {/* <p>Click the background to hide the designer</p> */}
+            </div>
             {createElement(app)}
           </div>
         </div>
