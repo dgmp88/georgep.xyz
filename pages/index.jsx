7c88268631558a2db780f4762d8f9a8cd93db897
George Prichard <dgmprichard@gmail.com>
2022-04-28 13:58:31 +0100

    Let next deal with the image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
 import { NavBar } from '../components/navbar';
 import { Background } from '../components/homeBackground';
+import Image from 'next/image';
+
+import mePic from '../public/images/me.jpg';
 
 function Home() {
   return (
@@ -9,11 +12,7 @@ function Home() {
       <div className="hero min-h-screen">
         <div className="hero-content prose text-center max-w-lg glass rounded-lg">
           <div>
-            <img
-              className="rounded-lg w-lg"
-              src="/images/me.jpg"
-              alt="George"
-            ></img>
+            <Image src={mePic} className="rounded-lg" />
           </div>
           <div className="">
             <h2 className="text-gray-100">Hi. I'm George</h2>
