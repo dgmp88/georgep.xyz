07bcc3f827b297e806c5ecd0e8db1dc94670afb3
George Prichard <dgmprichard@gmail.com>
2022-04-28 14:46:06 +0100

    Next bundle analyser for less js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 module.exports = {
   reactStrictMode: true,
+  withBundleAnalyzer: withBundleAnalyzer({}),
 };
