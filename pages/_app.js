a808a8158920b211a97c9ceba1a5fe50fa124381
George Prichard <dgmprichard@gmail.com>
2022-04-28 15:45:08 +0100

    Add in google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import 'tailwindcss/tailwind.css';
 import '../css/prism.css';
 import '../css/index.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-DZGP2WBCWD" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DZGP2WBCWD');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
