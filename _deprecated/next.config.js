e41061821a05dc5722385808f82748ffe0539e61
George Prichard <dgmprichard@gmail.com>
2025-12-06 15:47:47 +0000

    cleanup

--- _deprecated/next.config.js
+++
@@ -1,7 +0,0 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = {
-  reactStrictMode: true,
-  withBundleAnalyzer: withBundleAnalyzer({}),
-};
